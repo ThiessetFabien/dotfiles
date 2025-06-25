c232146ad67f11c1b94c7b2792639fd5b09c05f5
ThiessetFabien <thiessetfabienpro@gmail.com>
2025-06-25 15:34:36 +0200

    :wrend: add hyprland and waybar configs

--- script-backup.sh
+++
@@ -1,4 +0,0 @@
-BACKUP_DATE=$(date +%Y-%m-%d)
-BORG_REPO="Backup"
-borg create --stats --progress $BORG_REPO::backup-$BACKUP_DATE /etc /home
-borg prune -v --keep-daily=7 --keep-weekly=4 --keep-monthly=6 $BORG_REPO
