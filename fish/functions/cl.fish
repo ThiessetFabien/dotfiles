9902319ab4fd369a307b777f6aecc2db043a57c2
ThiessetFabien <thiessetfabienpro@gmail.com>
2025-06-27 22:53:17 +0200

    :wrench: adding alias fish for cursor

---
+++ fish/functions/cl.fish
@@ -0,0 +1,4 @@
+function cl --wraps='cursor . && lazygit' --description 'alias cl=cursor . && lazygit'
+  cursor . && lazygit $argv
+        
+end
---
+++ fish/functions/csr.fish
@@ -0,0 +1,4 @@
+function csr --wraps='cursor .' --description 'alias csr=cursor .'
+  cursor . $argv
+        
+end
