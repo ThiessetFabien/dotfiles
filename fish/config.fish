e103b83d1caeb1de3ba11579b8a711eb31825929
Thiesset Fabien <thiessetfabienpro@gmail.com>
2025-11-06 10:35:00 +0100

    🔧 update fish config with claude code

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,20 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
-    alias cat="bat"
-    alias ls="eza -la --icons"
-
-
 end
 
 # pnpm
 set -gx PNPM_HOME "/home/fabien/.local/share/pnpm"
+
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+# bun
+set -Ux PATH $HOME/.bun/bin $PATH
+
+# postgresql variable
+set -x PGUSER fabien
+
+export PATH="$HOME/.local/bin:$PATH"
